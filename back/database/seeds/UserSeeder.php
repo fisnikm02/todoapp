bca21e066ee58b0b764fc7cf3a45dc3a40e7112a
Fisnik Murati <fisnikmurati94@gmail.com>
2022-01-29 12:46:46 +0100

    seed added user

--- back/database/seeds/UserSeeder.php
+++ back/database/seeds/UserSeeder.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
@@ -12,15 +13,24 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        if(DB::table('users')->count()) {
+        if (DB::table('users')->count()) {
             return;
         }
-        DB::table('users')->insert([
-            'first_name' => 'Admin',
-            'last_name' => 'Admin',
-            'email' => 'administrator@email.com',
-            'password' => Hash::make('admin2021'),
-            'role' => 'admin'
+        \DB::table('users')->insert([
+            [
+                'first_name' => 'Admin',
+                'last_name' => 'Admin',
+                'email' => 'administrator@email.com',
+                'password' => Hash::make('admin2022'),
+                'role' => 'admin'
+            ],
+            [
+                'first_name' => 'User',
+                'last_name' => 'user',
+                'email' => 'user@email.com',
+                'password' => Hash::make('user2022'),
+                'role' => 'user'
+            ]
         ]);
     }
 }
